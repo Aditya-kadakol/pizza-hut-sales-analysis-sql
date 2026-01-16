fadffb678dca82cb5ec292c1248ed9da0c08aa2b
Aditya-kadakol <adityakadakol17@gmail.com>
2026-01-16 16:51:05 +0530

    Add database schema
    Initial database and table structure for Pizza Hut sales analysis

---
+++ database_setup/schema.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE pizzahut;
+USE pizzahut;
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    order_date DATE NOT NULL,
+    order_time TIME NOT NULL
+);
+
+CREATE TABLE order_details (
+    order_details_id INT PRIMARY KEY,
+    order_id INT NOT NULL,
+    pizza_id TEXT NOT NULL,
+    quantity INT NOT NULL
+);
